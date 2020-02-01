b4a1d2d04c1b6fc3bb582c9b943e7c0dd3d903bd
nballou <nick.ballou@gmail.com>
2020-02-01 16:46:44 +0000

    grid occupied by bot

--- gym_cool_game/envs/board.py
+++ gym_cool_game/envs/board.py
@@ -2,20 +2,19 @@ from gym_cool_game.envs.game import Bot
 
 import numpy as np
 
-
 class Board:
-    UP = 1
-    RIGHT = 2
-    DOWN = 3
-    LEFT = 4
+    LEFT = 1
+    DOWN = 2
+    RIGHT = 3
+    UP = 4
 
     def __init__(self, board_size, bot1, bot2):
         # self.grid = [[1 for x in range(int(board_size))] for y in range(int(board_size))]
         # self.grid[1:-1][1:-1] = 0
         self.grid = np.ones((board_size, board_size), dtype=int)
         self.grid[1:-1, 1:-1] = 0
-        # todo - make board boundaries 1s
-        # make occupied tiles occupied by object itself
+        self.grid = self.grid.tolist()
+
         self.bot1 = bot1
         self.bot2 = bot2
 
@@ -23,7 +22,7 @@ class Board:
     # Must be passed an object of class Bot, of course
     def set(self, bot, x, y):
         try: # In case the current location is off the grid
-            self.grid[bot.pos_x][bot.pos_y] = 0
+            self.grid[bot.pos_x][bot.pos_y] = bot
         except:
             None
 
@@ -47,22 +46,22 @@ class Board:
 
     # Move bot in direction
     def move(self, bot, direction):
-        if direction == 1 and bot.pos_y > 1:  # UP
+        if direction == 1 and bot.pos_y > 1:  # LEFT
             self.grid[bot.pos_x][bot.pos_y] = 0
             bot.pos_y -= 1
-            self.grid[bot.pos_x][bot.pos_y] = 2
-        elif direction == 2 and bot.pos_x < len(self.grid - 2):  # RIGHT
+            self.grid[bot.pos_x][bot.pos_y] = bot
+        elif direction == 2 and bot.pos_x < len(self.grid) - 2:  # DOWN
             self.grid[bot.pos_x][bot.pos_y] = 0
             bot.pos_x += 1
-            self.grid[bot.pos_x][bot.pos_y] = 2
-        elif direction == 3 and bot.pos_y < len(self.grid - 2):  # DOWN
+            self.grid[bot.pos_x][bot.pos_y] = bot
+        elif direction == 3 and bot.pos_y < len(self.grid) - 2:  # RIGHT
             self.grid[bot.pos_x][bot.pos_y] = 0
             bot.pos_y += 1
-            self.grid[bot.pos_x][bot.pos_y] = 2
-        elif direction == 4 and bot.pos_x > 1:  # LEFT
+            self.grid[bot.pos_x][bot.pos_y] = bot
+        elif direction == 4 and bot.pos_x > 1:  # UP
             self.grid[bot.pos_x][bot.pos_y] = 0
             bot.pos_x -= 1
-            self.grid[bot.pos_x][bot.pos_y] = 2
+            self.grid[bot.pos_x][bot.pos_y] = bot
 
 def test_print(grid):
     for i in range(0, len(grid)):
@@ -80,7 +79,7 @@ print()
 
 myboard.move(babyBot, 1)
 test_print(myboard.grid)
-print("Baby bot moved UP and is now located at: " + str(babyBot.pos_x) + "," + str(babyBot.pos_y))
+print("Baby bot moved LEFT and is now located at: " + str(babyBot.pos_y) + "," + str(babyBot.pos_x))
 print("Your valid moves are: " + str(myboard.get_valid_moves(babyBot)))
 
 myboard.set(babyBot, 4, 4)
@@ -91,5 +90,5 @@ print()
 
 myboard.move(babyBot, 2)
 test_print(myboard.grid)
-print("Baby bot moved RIGHT and is now located at: " + str(babyBot.pos_x) + "," + str(babyBot.pos_y))
+print("Baby bot moved DOWN and is now located at: " + str(babyBot.pos_y) + "," + str(babyBot.pos_x))
 print("Your valid moves are: " + str(myboard.get_valid_moves(babyBot)))
