967120779db703c2f6e597eb2a5c3210e72784df
hopshackle <james@janigo.co.uk>
2020-02-01 16:00:58 +0000

    MCTS branch added

---
+++ gym_cool_game/envs/RandomWalk.py
@@ -0,0 +1,79 @@
+from typing import List
+from copy import deepcopy
+import numpy as np
+import gym
+import pygame
+
+
+class RandomWalkEnv(gym.Env):
+    def __init__(self, target=3, current_positions=None):
+        # TODO: figure out params
+        if current_positions is None:
+            current_positions = [0, 0]
+        self.target = target
+        self.winner = 0
+        self.current_positions = current_positions
+
+    def reset(self):
+        self.winner = 0
+        self.current_positions = [0, 0]
+        return np.array([[], []])  # TODO: must return observation for each agent
+
+    def clone(self):
+        """
+        Creates a deep copy of the game state.
+        NOTE: it is _really_ important that a copy is used during simulations
+              Because otherwise MCTS would be operating on the real game board.
+        :returns: deep copy of this GameState
+        """
+        return RandomWalkEnv(target=self.target, current_positions=self.current_positions.copy())
+
+    def step(self, actions: List):
+        """
+        :param actions: List of two elements, containing one action for each player
+        """
+        for i in range(2):
+            if actions[i] == 0:
+                self.current_positions[i] += 1
+            elif actions[i] == 1:
+                self.current_positions[i] -= 1
+            else:
+                pass
+
+        reward_vector = [1 if self.target == p else 0 for p in self.current_positions]
+
+        # TODO: find if a player has won
+        if reward_vector[0] == 1:
+            self.winner = 1
+        elif reward_vector[1] == 1:
+            self.winner = 2
+        else:
+            self.winner = 0
+
+        # info should be kept empty
+        info = {}
+        return [[self.current_positions[0]], [self.current_positions[1]]], reward_vector, self.winner != 0, info
+
+    def get_moves(self):
+        """
+        :returns: array with all possible moves, index of columns which aren't full
+        TODO: figure out what are the valid moves an agent can take.
+        (i.e figure ability cooldowns / collision against map borders)
+        """
+        if self.winner != 0:
+            return []
+        return [0, 1]
+
+    def get_result(self, player):
+        """
+        :param player: (int) player which we want to see if he / she is a winner
+        :returns: winner from the perspective of the param player
+        """
+        return player == self.winner
+
+    def render(self, mode='human'):
+        # TODO: Here's where we would show the screen based on the game state
+        if mode == 'rgb':
+            # This might be useful
+            return pygame.surfarray.array3d(
+                pygame.display.get_surface()).astype(np.uint8)
--- gym_cool_game/envs/__init__.py
+++ gym_cool_game/envs/__init__.py
@@ -1,1 +1,8 @@
 from gym_cool_game.envs.cool_game_env import CoolGameEnv
+
+from gym.envs.registration import register
+
+register(id="RandomWalk-v0", entry_point='gym_cool_game.envs.RandomWalk:RandomWalkEnv',
+         max_episode_steps=20, reward_threshold=1.0)
+
+# gym.make("RandomWalk-v0", target=2) is then relevant make command
