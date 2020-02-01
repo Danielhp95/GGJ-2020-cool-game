b1baae9d185945922b1d2f62cf5bfeaac4870194
nballou <nick.ballou@gmail.com>
2020-02-01 18:05:51 +0000

    2p move function, clean up unnecessary code

--- gym_cool_game/envs/board.py
+++ gym_cool_game/envs/board.py
@@ -2,33 +2,36 @@ from gym_cool_game.envs.game import Bot
 
 import numpy as np
 
-class Board:
-    LEFT = 1
-    DOWN = 2
-    RIGHT = 3
-    UP = 4
 
-    def __init__(self, board_size, bot1, bot2):
-        # self.grid = [[1 for x in range(int(board_size))] for y in range(int(board_size))]
-        # self.grid[1:-1][1:-1] = 0
+class Board:
+
+    def __init__(self, board_size):
         self.grid = np.ones((board_size, board_size), dtype=int)
         self.grid[1:-1, 1:-1] = 0
         self.grid = self.grid.tolist()
 
-        self.bot1 = bot1
-        self.bot2 = bot2
-
     # Set the position of bot to (x, y)
     # Must be passed an object of class Bot, of course
     def set(self, bot, x, y):
-        try: # In case the current location is off the grid
-            self.grid[bot.pos_x][bot.pos_y] = bot
-        except:
-            None
 
-        bot.pos_x = x
-        bot.pos_y = y
-        self.grid[x][y] = 2  # set new tile on grid to occupied
+        # Only attempt to set current position to 0 if bot is already on the board
+        # This becomes irrelevant if bots are only ever instantiated in valid locations
+        if self.currently_on_board(bot.pos_x, bot.pos_y) == True:
+            self.grid[bot.pos_x][bot.pos_y] = 0
+            bot.pos_x = x
+            bot.pos_y = y
+            self.grid[x][y] = bot  # set new tile on grid to occupied
+        else:
+            bot.pos_x = x
+            bot.pos_y = y
+            self.grid[x][y] = bot
+
+    # Check if a set of coordinates is on the board
+    def currently_on_board(self, x, y):
+        if x > len(self.grid)-1 or x < 0 or y > len(self.grid)-1 or y < 0:
+            return False
+        else:
+            return True
 
     # Get a list of valid directional moves for bot
     def get_valid_moves(self, bot):
@@ -44,51 +47,81 @@ class Board:
 
         return valid_moves
 
-    # Move bot in direction
-    def move(self, bot, direction):
-        if direction == 1 and bot.pos_y > 1:  # LEFT
-            self.grid[bot.pos_x][bot.pos_y] = 0
-            bot.pos_y -= 1
-            self.grid[bot.pos_x][bot.pos_y] = bot
-        elif direction == 2 and bot.pos_x < len(self.grid) - 2:  # DOWN
-            self.grid[bot.pos_x][bot.pos_y] = 0
-            bot.pos_x += 1
-            self.grid[bot.pos_x][bot.pos_y] = bot
-        elif direction == 3 and bot.pos_y < len(self.grid) - 2:  # RIGHT
-            self.grid[bot.pos_x][bot.pos_y] = 0
-            bot.pos_y += 1
-            self.grid[bot.pos_x][bot.pos_y] = bot
-        elif direction == 4 and bot.pos_x > 1:  # UP
-            self.grid[bot.pos_x][bot.pos_y] = 0
-            bot.pos_x -= 1
-            self.grid[bot.pos_x][bot.pos_y] = bot
 
-def test_print(grid):
-    for i in range(0, len(grid)):
-        print(str(grid[i]))
+    def resolve_moves(self, bot1, direction1, bot2, direction2):
 
-# Testing stuff
-babyBot = Bot()
-mamaBot = Bot()
-myboard = Board(10, babyBot, mamaBot)
+        # Figure out where bot1 is moving to
+        if direction1 == "LEFT" and bot1.pos_y > 1 and self.grid[bot1.pos_x][bot1.pos_y-1] == 0:  # LEFT
+            bot1.next_move_y = bot1.pos_y - 1
+            bot1.next_move_x = bot1.pos_x
+
+        elif direction1 == "DOWN" and bot1.pos_x < len(self.grid) - 2 and self.grid[bot1.pos_x+1][bot1.pos_y] == 0:  # DOWN
+            bot1.next_move_x = bot1.pos_x + 1
+            bot1.next_move_y = bot1.pos_y
+
+        elif direction1 == "RIGHT" and bot1.pos_y < len(self.grid) - 2 and self.grid[bot1.pos_x][bot1.pos_y+1] == 0:  # RIGHT
+            bot1.next_move_y = bot1.pos_y + 1
+            bot1.next_move_x = bot1.pos_x
+
+        elif direction1 == "UP" and bot1.pos_x > 1 and self.grid[bot1.pos_x-1][bot1.pos_y] == 0:  # UP
+            bot1.next_move_x = bot1.pos_x - 1
+            bot1.next_move_y = bot1.pos_y
+
+        # Figure out where bot2 is moving to
+        if direction2 == "LEFT" and bot2.pos_y > 1 and self.grid[bot2.pos_x][bot2.pos_y-1] == 0:  # LEFT
+            bot2.next_move_y = bot2.pos_y - 1
+            bot2.next_move_x = bot2.pos_x
+
+        elif direction2 == "DOWN" and bot2.pos_x < len(self.grid) - 2 and self.grid[bot2.pos_x+1][bot2.pos_y] == 0:  # DOWN
+            bot2.next_move_x = bot2.pos_x + 1
+            bot2.next_move_y = bot2.pos_y
+
+        elif direction2 == "RIGHT" and bot2.pos_y < len(self.grid) - 2 and self.grid[bot2.pos_x][bot2.pos_y+1] == 0:  # RIGHT
+            bot2.next_move_y = bot2.pos_y + 1
+            bot2.next_move_x = bot2.pos_x
+
+        elif direction2 == "UP" and bot2.pos_x > 1 and self.grid[bot2.pos_x-1][bot2.pos_y] == 0:  # UP
+            bot2.next_move_x = bot2.pos_x - 1
+            bot2.next_move_y = bot2.pos_y
+
+        # If the two spaces where they're moving to is not the same, move them there
+        if [bot1.next_move_x, bot1.next_move_y] != [bot2.next_move_x, bot2.next_move_y]:
+            self.set(bot1, bot1.next_move_x, bot1.next_move_y)
+            self.set(bot2, bot2.next_move_x, bot2.next_move_y)
+
+        # If the bots are moving to the same space, move only the one with heavier weight
+        elif [bot1.next_move_x, bot1.next_move_y] == [bot2.next_move_x, bot2.next_move_y]:
+
+            if bot1.weight >= bot2.weight:
+                print("*************** COLLISION, bot 1 is heavier *********************")
+                self.set(bot1, bot1.next_move_x, bot1.next_move_y)
+            else:
+                print("*************** COLLISION, bot 2 is heavier *********************")
+                self.set(bot2, bot2.next_move_x, bot2.next_move_y)
+
+        self.test_print()
+
+    def test_print(self):
+        for i in range(0, len(self.grid)):
+            print(str(self.grid[i]))
+
+
+# ---------------------
+# Super janky testing stuff because I don't know any better
+# --------------------
+
+babyBot = Bot(20)
+mamaBot = Bot(30)
+myboard = Board(10)
 
 myboard.set(babyBot, 1, 1)
-test_print(myboard.grid)
-print("Baby bot is currently located at: " + str(babyBot.pos_x) + "," + str(babyBot.pos_y))
+myboard.set(mamaBot, 1, 5)
+
+myboard.test_print()
 print()
 
-myboard.move(babyBot, 1)
-test_print(myboard.grid)
-print("Baby bot moved LEFT and is now located at: " + str(babyBot.pos_y) + "," + str(babyBot.pos_x))
-print("Your valid moves are: " + str(myboard.get_valid_moves(babyBot)))
-
-myboard.set(babyBot, 4, 4)
-test_print(myboard.grid)
-print("Baby bot is now located at: " + str(babyBot.pos_x) + "," + str(babyBot.pos_y))
-print("Your valid moves are: " + str(myboard.get_valid_moves(babyBot)))
+myboard.resolve_moves(babyBot, "RIGHT", mamaBot,  "LEFT")
 print()
 
-myboard.move(babyBot, 2)
-test_print(myboard.grid)
-print("Baby bot moved DOWN and is now located at: " + str(babyBot.pos_y) + "," + str(babyBot.pos_x))
-print("Your valid moves are: " + str(myboard.get_valid_moves(babyBot)))
+myboard.resolve_moves(babyBot, "RIGHT", mamaBot,  "LEFT")
+
