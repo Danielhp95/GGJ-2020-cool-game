48d09f1cc16459459135609e0de596b36aa78f0e
Daniel Hernandez <danielhp95@gmail.com>
2020-03-10 18:17:13 +0000

    Update and rename clear_and_run_parallel_experiment.sh to clear_mongo_generated_files.sh
    This script no longer starts balancing experiment, only removes stuff

---
+++ hyperopt_mongo/clear_mongo_generated_files.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -rf sizeStorer.wt WiredTiger WiredTigerLAS.wt WiredTiger.lock WiredTiger.turtle WiredTiger.wt storage.bson _mdb_catalog.wt local logfile.txt journal admin config diagnostic.data foo_db mongod.lock
