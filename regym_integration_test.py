45dc0a72656df555335d1d9e1e4f465a810d914c
daniel <danielhp95@gmail.com>
2020-03-17 09:09:26 +0000

    removed dependency with regym.rl_algorithms.agents.HumanAgent

--- regym_integration_test.py
+++ regym_integration_test.py
@@ -1,8 +1,21 @@
 import gym_cool_game
 import regym
 from regym.environments import generate_task, EnvType
-from regym.rl_algorithms import build_Random_Agent, build_MCTS_Agent, build_Human_Agent
+from regym.rl_algorithms import build_Random_Agent, build_MCTS_Agent
 
+class HumanAgent:
+
+    def __init__(self, number_of_actions: int, name: str):
+        self.name = name
+        self.number_of_actions = number_of_actions
+
+    def take_action(self, state):
+        action = input(f'Take action [0, {self.number_of_actions}]: ')
+        if action.isnumeric() and int(action) in range(0, 6):
+            return int(action)
+        else:
+            print("Invalid entry. Defaulting to null action.")
+            return 5
 
 def main():
     task = generate_task('CoolGame-v0', EnvType.MULTIAGENT_SIMULTANEOUS_ACTION,
@@ -10,11 +23,11 @@ def main():
     random_r1 = build_Random_Agent(task, {}, agent_name='random')
     random_r2 = random_r1.clone()
 
-    mcts_r1 = build_MCTS_Agent(task, {'budget': 5000}, agent_name='P1: MCTS')
-    mcts_r2 = build_MCTS_Agent(task, {'budget': 5000}, agent_name='P2: MCTS')
+    mcts_r1 = build_MCTS_Agent(task, {'budget': 2000}, agent_name='P1: MCTS')
+    mcts_r2 = build_MCTS_Agent(task, {'budget': 200}, agent_name='P2: MCTS')
 
-    human_r1 = build_Human_Agent(task, {}, agent_name='P1')
-    human_r2 = build_Human_Agent(task, {}, agent_name='P2')
+    human_r1 = HumanAgent(5, 'P1')
+    human_r2 = HumanAgent(5, 'P2')
     #t = task.run_episode([human_r1, human_r2], training=False, render_mode='rgb')
     t = task.run_episode([mcts_r1, mcts_r2], training=False, render_mode='rgb')
     print(t)
