cec003f73288faee40ed6c00a5fa5b68bc85dc98
daniel <danielhp95@gmail.com>
2020-04-01 10:39:41 +0100

    Added rollout_budget and passed flag to save_gif

--- regym_integration_test.py
+++ regym_integration_test.py
@@ -21,18 +21,18 @@ class HumanAgent:
 
 def main():
     task = generate_task('CoolGame-v0', EnvType.MULTIAGENT_SIMULTANEOUS_ACTION,
-                         botA_type=0, botB_type=2)
+                         botA_type=1, botB_type=2)
 
     random_r1 = build_Random_Agent(task, {}, agent_name='random')
     random_r2 = random_r1.clone()
 
-    mcts_r1 = build_MCTS_Agent(task, {'budget': 1296}, agent_name='P1: MCTS')
-    mcts_r2 = build_MCTS_Agent(task, {'budget': 1296}, agent_name='P2: MCTS')
+    mcts_r1 = build_MCTS_Agent(task, {'budget': 625, 'rollout_budget': 10}, agent_name='P1: MCTS')
+    mcts_r2 = build_MCTS_Agent(task, {'budget': 625, 'rollout_budget': 10}, agent_name='P2: MCTS')
 
     human_r1 = HumanAgent(task.action_dim, name='P1')
     human_r2 = HumanAgent(task.action_dim, name='P2')
 
-    t = task.run_episode([mcts_r1, mcts_r2], training=False, render_mode='rgb')
+    t = task.run_episode([mcts_r1, mcts_r2], training=False, render_mode='rgb', save_gif=True)
 
     print(t)
 
